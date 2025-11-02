494d03065a10c6dc02a00030ce5133488a820844
Asafe-cod <asafefelipe0507@gmail.com>
2025-11-02 22:16:08 +0000

    Criei um printf

---
+++ desafiocartas.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+ int main(){
+     /*DADOS CARTA 1*/
+    char estado[]= "A" ;   
+    char codigo[]= "A01";
+    char cidade[]= "Fortaleza";
+    int populacao= 20.000;
+    float area= 10.000;
+    float pib= 30.000;
+    int turisticos= 50;
+
+      /*DADOS PARA EXPOR*/
+
+     printf("Estado: %s\n",estado);
+     printf("Código:%s\n",codigo);
+     printf("Cidade:%s\n",cidade);
+     printf("População:%d\n",populacao);
+     printf("Área:%f\n",area);
+     printf("PIB:%f\n",pib);
+     printf("Pontos turísticos:%d\n",turisticos);
+
+
+     /* DADOS CARTA 2*/
+     
+     char estado2[]= "B" ;   
+    char codigo2[]= "B02";
+    char cidade2[]= "Recife";
+    int populacao2= 70.000;
+    float area2= 90.000;
+    float pib2= 60.000;
+    int turisticos2= 20;
+
+
+    /*DADOS PARA EXPOR*/
+
+     printf("Estado: %s\n",estado2);
+     printf("Código:%s\n",codigo2);
+     printf("Cidade:%s\n",cidade2);
+     printf("População:%d\n",populacao2);
+     printf("Área:%f\n",area2);
+     printf("PIB:%f\n",pib2);
+     printf("Pontos turísticos:%d\n",turisticos2);
+
+
