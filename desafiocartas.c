d6431eda91b5a4e2b3e0d520d05ddbe5926feeb5
Asafe-cod <asafefelipe0507@gmail.com>
2025-11-12 20:45:31 +0000

    adicionei novos contéudos as cartas

--- desafiocartas.c
+++ desafiocartas.c
@@ -2,6 +2,7 @@
  int main(){
      printf("desafio cartas\n");
     /*DADOS CARTA 1*/
+    char nome[30]= "carta1";
     char estado[]= "A" ;   
     char codigo[]= "A01";
     char cidade[]= "Fortaleza";
@@ -11,6 +12,7 @@
     int turisticos= 50;
     float densidade=populacao/area;
     float percapita=pib/populacao;
+    float superpoder=populacao+area+pib + densidade;
 
       /*DADOS PARA EXPOR*/
 
@@ -26,9 +28,11 @@
 
 
 
+
      /* DADOS CARTA 2*/
      
-     char estado2[]= "B" ;   
+    char nome2[30]="carta2";
+    char estado2[]= "B" ;   
     char codigo2[]= "B02";
     char cidade2[]= "Recife";
     int populacao2= 70.000;
@@ -37,6 +41,7 @@
     int turisticos2= 20;
     float densidade2=populacao2/area2;
     float percapita2= pib2/populacao2;
+    float superpoder2=populacao2+area2+pib2 + densidade2;
 
 
     /*DADOS PARA EXPOR*/
@@ -50,7 +55,15 @@
      printf("Pontos turísticos:%d\n",turisticos2);
      printf("Densidade populacional: %f\n",densidade2);
      printf("PIB per capita:%f\n",percapita2);
+     printf("Super poder 1:%f\n",superpoder);
+     printf("Super poder 1:%f\n",superpoder2);
+     printf("A População da carta 1 é maior que população2:%d\n",populacao>populacao2);
+     printf("O pib da carta1 é maior que o pib da carta 2::%d\n",pib > pib2);
+     printf("A Densidade populacional da carta 1 maior que o da carta 2::%d\n",densidade> densidade2);
+     printf("A Area da carta 1 é maior que o da carta2:%d\n",area>area2);
+     printf("A carta1 é mais forte que a carta2:%d\n", superpoder>superpoder2);
 
 
-
+     
+    return 0;
 }
